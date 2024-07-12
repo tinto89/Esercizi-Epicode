534b44fb25ca440d39352f8eca334b43504360d9
tinto89 <tinto89@hotmail.it>
2024-07-12 21:54:08 +0200

    M2 W1D4

--- M2/W1D4-JsBasics/s1p2-scaricabile.js
+++ M2/W1D4-JsBasics/s1p2-scaricabile.js
@@ -27,35 +27,34 @@ REGOLE
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.*/
 
- let value = 12 + 20;
+let value = 12 + 20;
 
- /* ESERCIZIO 4
+/* ESERCIZIO 4
   Crea una variable di nome "x" e assegna ad essa il numero 12.*/
- 
- let x = 12;
- 
- /* ESERCIZIO 5
+
+let x = 12;
+
+/* ESERCIZIO 5
   Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.*/
- 
- let name = "Gabriele";
- 
- /* ESERCIZIO 6
+
+let name = "Gabriele";
+
+/* ESERCIZIO 6
   Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).*/
- 
- let value2 = 4 - x;
- console.log(value2) = -8
- 
- 
- /* ESERCIZIO 7
+
+let value2 = 4 - x;
+console.log(value2); // -8
+
+/* ESERCIZIO 7
   Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
   Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
   Infine, verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
   NON HAI BISOGNO DI UN BLOCCO IF/ELSE. E' sufficiente utilizzare console.log().
  */
- 
- let name1 = "john";
- let name2 = "John";
- console.log(name1 === name2); //false
- name1 = name1.toLowerCase();
- name2 = name2.toLowerCase();
- console.log(name1 === name2); //true
+
+let name1 = "john";
+let name2 = "John";
+console.log(name1 === name2); //false
+name1 = name1.toLowerCase();
+name2 = name2.toLowerCase();
+console.log(name1 === name2); //true
